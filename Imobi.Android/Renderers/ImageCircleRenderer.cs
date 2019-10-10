c0d4d7a7ebe5f3e0e1dae2aa60e4db931aac1e87
Oziel Guimarães de Paula Silva <oziel.paula.silva@everis.com>
2019-10-10 18:16:40 -0300

    Added custom render for image circle

---
+++ Imobi.Android/Renderers/ImageCircleRenderer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Views;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using System.ComponentModel;
+using Imobi.Views.Components;
+using Imobi.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(ImageCircle), typeof(ImageCircleRenderer))]
+
+namespace Imobi.Droid.Renderers
+{
+    public class ImageCircleRenderer : ImageRenderer
+    {
+        public ImageCircleRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                if ((int)Android.OS.Build.VERSION.SdkInt < 18)
+                    SetLayerType(LayerType.Software, null);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == ImageCircle.BorderColorProperty.PropertyName ||
+              e.PropertyName == ImageCircle.BorderThicknessProperty.PropertyName ||
+              e.PropertyName == ImageCircle.FillColorProperty.PropertyName)
+            {
+                Invalidate();
+            }
+        }
+
+        protected override bool DrawChild(Canvas canvas, global::Android.Views.View child, long drawingTime)
+        {
+            try
+            {
+                var radius = (float)Math.Min(Width, Height) / 2f;
+                var borderThickness = ((ImageCircle)Element).BorderThickness;
+                var strokeWidth = 0f;
+
+                if (borderThickness > 0)
+                {
+                    var logicalDensity = Android.App.Application.Context.Resources.DisplayMetrics.Density;
+                    strokeWidth = (float)Math.Ceiling(borderThickness * logicalDensity + .5f);
+                }
+                radius -= strokeWidth / 2f;
+
+                var path = new Path();
+                path.AddCircle(Width / 2.0f, Height / 2.0f, radius, Path.Direction.Ccw);
+                canvas.Save();
+                canvas.ClipPath(path);
+
+                var paint = new Paint
+                {
+                    AntiAlias = true
+                };
+                paint.SetStyle(Paint.Style.Fill);
+                paint.Color = ((ImageCircle)Element).FillColor.ToAndroid();
+                canvas.DrawPath(path, paint);
+                paint.Dispose();
+
+                var result = base.DrawChild(canvas, child, drawingTime);
+
+                path.Dispose();
+                canvas.Restore();
+
+                path = new Path();
+                path.AddCircle(Width / 2f, Height / 2f, radius, Path.Direction.Ccw);
+
+                if (strokeWidth > 0.0f)
+                {
+                    paint = new Paint
+                    {
+                        AntiAlias = true,
+                        StrokeWidth = strokeWidth
+                    };
+                    paint.SetStyle(Paint.Style.Stroke);
+                    paint.Color = ((ImageCircle)Element).BorderColor.ToAndroid();
+                    canvas.DrawPath(path, paint);
+                    paint.Dispose();
+                }
+
+                path.Dispose();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Unable to create circle image: " + ex);
+            }
+            return base.DrawChild(canvas, child, drawingTime);
+        }
+    }
+}
---
+++ Imobi.iOS/Renderers/ImageCircleRenderer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using Imobi.iOS.Renderers;
+using Imobi.Views.Components;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using System.Linq;
+using CoreAnimation;
+using CoreGraphics;
+using System.Diagnostics;
+
+[assembly: ExportRenderer(typeof(ImageCircle), typeof(ImageCircleRenderer))]
+
+namespace Imobi.iOS.Renderers
+{
+    internal class ImageCircleRenderer : ImageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null) return;
+
+            CreateCircle();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName ||
+                e.PropertyName == VisualElement.WidthProperty.PropertyName ||
+              e.PropertyName == ImageCircle.BorderColorProperty.PropertyName ||
+              e.PropertyName == ImageCircle.BorderThicknessProperty.PropertyName ||
+              e.PropertyName == ImageCircle.FillColorProperty.PropertyName)
+            {
+                CreateCircle();
+            }
+        }
+
+        private void CreateCircle()
+        {
+            try
+            {
+                var min = Math.Min(Element.Width, Element.Height);
+                Control.Layer.CornerRadius = (nfloat)(min / 2.0);
+                Control.Layer.MasksToBounds = false;
+                Control.BackgroundColor = ((ImageCircle)Element).FillColor.ToUIColor();
+                Control.ClipsToBounds = true;
+
+                var borderThickness = ((ImageCircle)Element).BorderThickness;
+
+                //Remove previously added layers
+                var tempLayer = Control.Layer.Sublayers?
+                                       .Where(p => p.Name == borderName)
+                                       .FirstOrDefault();
+                tempLayer?.RemoveFromSuperLayer();
+
+                var externalBorder = new CALayer
+                {
+                    Name = borderName,
+                    CornerRadius = Control.Layer.CornerRadius,
+                    Frame = new CGRect(-.5, -.5, min + 1, min + 1),
+                    BorderColor = ((ImageCircle)Element).BorderColor.ToCGColor(),
+                    BorderWidth = ((ImageCircle)Element).BorderThickness
+                };
+
+                Control.Layer.AddSublayer(externalBorder);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Unable to create circle image: " + ex);
+            }
+        }
+
+        private const string borderName = "borderLayerName";
+    }
+}
